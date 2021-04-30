fea6c17a50513a34993223ee43e85b4a330d2eff
Pedro Lucas <pedro.lima3@estudante.ifms.edu.br>
2021-04-30 03:05:40 -0300

    segundo commit

--- Aula115/Entities/Order.cs
+++ Aula115/Entities/Order.cs
@@ -6,5 +6,9 @@ namespace Aula115.Entities {
         public int Id { get; set; }
         public DateTime Moment { get; set; }
         public OrderStatus Status { get; set; }
+
+        public override string ToString() {
+            return Id + ", " + Moment + ", " + Status;
+        }
     }
 }
--- Aula115/Program.cs
+++ Aula115/Program.cs
@@ -1,9 +1,31 @@
 ﻿using System;
+using Aula115.Entities;
+using Aula115.Entities.Enums;
 
 namespace Aula115 {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Enumeração:\n");
+
+            Order order = new Order {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            //Converter enum para string:
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            Console.WriteLine(txt);
+
+            //Ao contrário, para se converter uma string para enum:
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os);
         }
     }
 }
