3206f6f7e67350bb0373e0896b7f816288dad633
Pedro Lucas <pedro.lima3@estudante.ifms.edu.br>
2021-04-30 02:34:04 -0300

    first commit

---
+++ Aula115/Entities/Order.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Aula115.Entities.Enums;
+
+namespace Aula115.Entities {
+    class Order {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+    }
+}
---
+++ Aula115/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Aula115 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Enumeração:\n");
+        }
+    }
+}
